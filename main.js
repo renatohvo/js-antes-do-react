db89a03d90f019784bf80c22065426511183af27
renatohvo <renatohvo@gmail.com>
2024-01-08 18:51:49 -0300

    short syntax & optional chain

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document.body.innerHTML = '<h1>JS - Antes do React</h1>';
 const nome = null;
 document.body.innerHTML += '<p>Seu nome é: ' + (nome ?? 'Não informado!') + '</p>';
 
-//Objeto
+// Objeto
 const user = {
   name: 'Maria João',
   idade: 27,
@@ -33,3 +33,27 @@ document.body.innerHTML += '<p>' + JSON.stringify(rest) + '</p>';
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const [first, ,third, ...res] = array;
 document.body.innerHTML += '<p>' + JSON.stringify({ first, third, res }) + '</p>';
+
+// Short Syntax
+const employeeName = 'Renato Henrique';
+const employeeAge = 30;
+const employee = {
+  employeeName,
+  employeeAge,
+  address: {
+    street: 'Rua Teste',
+    number: 176,
+    zip: {
+      // code: '12312123',
+      city: 'Rio Grande'
+    },
+    // showFullAddress() {
+    //   return 'ok';
+    // }
+  },
+};
+document.body.innerHTML += '<p>' + JSON.stringify({ employee }) + '</p>';
+
+// Optional Chain
+document.body.innerHTML += '<p>' + JSON.stringify(employee.address?.zip?.code ?? 'Não Informado') + '</p>';
+document.body.innerHTML += '<p>' + JSON.stringify(employee.address?.showFullAddress?.()) + '</p>';
